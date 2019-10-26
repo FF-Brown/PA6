e4b5c939ba94ce96bbdcc28bb208990a471877df
Nathan Brown <nathan.g.brown@wsu.edu>
2019-10-25 21:47:30 -0700

    Add project files.

---
+++ PA6/battleship.c
@@ -0,0 +1,8 @@
+/*
+	Author: Nathan Brown
+	Created: 10/25/2019
+	Description: The classic game of Battleship in text-based format versus a computer. Player is allowed to choose manual or random placement of ships, and difficulty of computer player. A log file is created detailing gameplay.
+*/
+
+#include "battleship.h"
+
---
+++ PA6/battleship.h
@@ -0,0 +1,12 @@
+/*
+	Author: Nathan Brown
+	Created: 10/25/2019
+	Description: The classic game of Battleship in text-based format versus a computer. Player is allowed to choose manual or random placement of ships, and difficulty of computer player. A log file is created detailing gameplay.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
